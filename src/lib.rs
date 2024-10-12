587f84015ae9a9919bcd8dc336a622f9657bfda8
waman <waman10da@gmail.com>
2024-10-12 09:05:04 +0900

    Add mod polynomial to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
 
 mod safe_i64;
 mod real;
+mod polynomial;
+// mod algebraic;
 
 #[cfg(test)]
 mod safe_i64_test;
+
+#[cfg(test)]
+mod real_test;
+
+#[cfg(test)]
+mod polynomial_test;
+
+// #[cfg(test)]
+// mod algebraic_test;
